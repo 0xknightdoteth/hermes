9d7d8ffcd7e32a5ad6f88128f85b4197fa966382
0xknightdoteth <0xknight@null0x0.io>
2022-12-25 11:46:01 -0800

    [feat] receive opcode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+        });
+    }
+}
+
+async fn handle_connection(mut socket: TcpStream) {
+    let mut buffer = [0; 2];
+    socket.read(&mut buffer).await.unwrap();
+
+    // convert buffer to opcode
+    let opcode = RecvOpcode::from_u16(u16::from_le_bytes(buffer)).unwrap();
+    println!("Opcode: {:?}", opcode);
+}
+
+
+#[derive(Debug)]
+enum RecvOpcode {
+    Ping = 1,
+}
+
+impl RecvOpcode {
+    fn from_u16(value: u16) -> Option<RecvOpcode> {
+        match value {
+            1 => Some(RecvOpcode::Ping),
+            _ => None,
+        }
+    }
 }
